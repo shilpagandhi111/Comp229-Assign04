98e05e42833d4a9d3d518f21bf4095311d026915
shilpa <shilpagandhi111@gmail.com>
2016-12-10 19:07:46 -0500

    email functionality fixed

--- Comp229-Assign04/Global.asax.cs
+++ Comp229-Assign04/Global.asax.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Mail;
+using System.Text;
 using System.Web;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -11,6 +13,7 @@ namespace Comp229_Assign04
 {
     public class Global : HttpApplication
     {
+       /// string MessageText = Encoding.ASCII.GetString(bytes);
         public static List<Models.Model.modelsData> models;
         private const string updatedJsonFilePath = @"C:\Users\shilp\Documents\Visual Studio 2015\Projects\Comp229-Assign04\Comp229-Assign04\Models\Assign04(1)Copy.json";
         void Application_Start(object sender, EventArgs e)
@@ -22,9 +25,34 @@ namespace Comp229_Assign04
             
         }
 
-        internal static void Email(string text1, string text2, string text3)
+        public static void Email(string clientEmailAddress, string clientName)
         {
-            throw new NotImplementedException();
+            SmtpClient smtpClient = new SmtpClient("smtp-mail.outlook.com", 587);
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+         
+            MailMessage message = new MailMessage();
+            try
+            {
+                MailAddress fromAddress = new MailAddress("cc-comp229f2016@outlook.com", "Comp229-Assign04");
+              /////Cannot make th message item to work due to the conversion of string
+                ////MailAddress toAddress = new MailAddress(clientEmailAddress, clientName,MessageText);
+                MailAddress toAddress = new MailAddress(clientEmailAddress, clientName);
+                message.From = fromAddress;
+                message.To.Add(toAddress);
+                message.Subject = "Comp229-Assign04 email";
+                message.Body = "using same message body for each email";
+
+                smtpClient.Host = "smtp-mail.outlook.com";
+                smtpClient.EnableSsl = true;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new System.Net.NetworkCredential("cc-comp229f2016@outlook.com", "comp229pwd");
+                smtpClient.Send(message);
+              
+            }
+            catch (Exception e)
+            {
+              
+            }
         }
 
         private void DisplayData()
